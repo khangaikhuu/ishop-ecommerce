f5b77cb7fc2a1af3bf99704cf079a9b965921912
TurbatBay <learnwithturu@gmail.com>
2023-03-20 20:24:21 +0800

    adding comments on backend code

--- e-commerce-backend/src/app.js
+++ e-commerce-backend/src/app.js
@@ -1,28 +1,33 @@
-console.log("iShop E-Commerce Backend");
+// Print message to console
+console.log('iShop E-Commerce Backend')
 
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import menuApi from "./routes/menu-api";
-import adminApi from "./routes/admin-api";
+// Import dependencies
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import menuApi from './routes/menu-api'
+import adminApi from './routes/admin-api'
 
-const app = express();
-const PORT = 8080;
+// Create Express app instance and set port number and MongoDB connection string
+const app = express()
+const PORT = 8080
 const MONGODB_CONNECTION_STRING =
-  "mongodb+srv://xxxx:xxxx@mern-black.kosj6y7.mongodb.net/ishop";
+  'mongodb+srv://MongoTuru:kIbRLDD0euIjTlRh@turumongo.pfftwk3.mongodb.net/test'
 
-app.use(cors());
-app.use(express.json());
-app.use("/menu", menuApi);
-app.use("/admin", adminApi);
+// Middleware functions
+app.use(cors()) // Enable Cross-Origin Resource Sharing
+app.use(express.json()) // Parse incoming JSON payloads
+app.use('/menu', menuApi) // Handle /menu API routes
+app.use('/admin', adminApi) // Handle /admin API routes
 
+// Start server and connect to MongoDB database
 app.listen(PORT, () => {
   mongoose
-    .connect(MONGODB_CONNECTION_STRING)
-    .then(console.log("Database successfully connected"))
-    .catch((error) => console.error(error));
+    .connect(MONGODB_CONNECTION_STRING) // Connect to MongoDB database
+    .then(console.log('Database successfully connected')) // Log successful connection
+    .catch((error) => console.error(error)) // Log error if connection fails
 
   console.log(
     `iSHop E-Commerce application is running on http://localhost:${PORT}`
-  );
-});
+  ) // Log message indicating server is running and accessible at specified URL
+})
